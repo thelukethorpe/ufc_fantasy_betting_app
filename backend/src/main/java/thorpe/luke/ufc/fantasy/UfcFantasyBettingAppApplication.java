255a14f8f2536f777237f4707c7dda460eee421f
Luke  Thorpe <luke.thorpe18@imperial.ac.uk>
2021-06-26 21:38:41 +0100

    Initial commit

---
+++ backend/src/main/java/thorpe/luke/ufc/fantasy/UfcFantasyBettingAppApplication.java
@@ -0,0 +1,13 @@
+package thorpe.luke.ufc.fantasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfcFantasyBettingAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UfcFantasyBettingAppApplication.class, args);
+  }
+
+}
---
+++ backend/src/test/java/thorpe/luke/ufc/fantasy/UfcFantasyBettingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package thorpe.luke.ufc.fantasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfcFantasyBettingAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
