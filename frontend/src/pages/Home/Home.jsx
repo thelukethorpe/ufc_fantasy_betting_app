aa18dd9c3df5f1601af8384f7c85314fb9663480
Luke  Thorpe <luke.thorpe18@imperial.ac.uk>
2021-07-03 14:22:18 +0100

    Setup basic carousel on home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,10 +4,15 @@ import {
   Button,
   IconButton,
   makeStyles,
+  Paper,
   Toolbar,
   Typography
 } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
+import Carousel from 'react-material-ui-carousel'
+import CarouselImage1 from './carousel_1.png'
+import CarouselImage2 from './carousel_2.png'
+import CarouselImage3 from './carousel_3.png'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,11 +23,42 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1
+  },
+  carousel: {
+    width: '80%',
+    margin: 'auto',
+    paddingTop: theme.spacing(10)
+  },
+  carouselItem: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundPosition: '50% 25%',
+    backgroundSize: 'cover',
+    height: '50vh'
   }
 }))
 
 const Home = () => {
   const classes = useStyles()
+  const carouselItems = [
+    {
+      name: 'UFC Fantasy Betting',
+      description: 'Coming soon',
+      image: `url(${CarouselImage1})`
+    },
+    {
+      name: 'Bet on real UFC events with virtual money',
+      description: 'Betting odds updated hourly',
+      image: `url(${CarouselImage2})`
+    },
+    {
+      name: 'Start with $1500',
+      description: '',
+      image: `url(${CarouselImage3})`
+    }
+  ]
 
   return (
     <div>
@@ -39,7 +75,26 @@ const Home = () => {
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+      <Carousel className={classes.carousel}>
+        {
+          carouselItems.map((item, i) => <CarouselItem key={i} item={item}
+                                                       className={classes.carouselItem}/>)
+        }
+      </Carousel>
     </div>
+  )
+}
+
+const CarouselItem = (props) => {
+  return (
+    <Paper className={props.className} maxWidth="1920px"
+           style={{ backgroundImage: props.item.image }}>
+      <Typography variant="h1" align="center"
+                  color="secondary">{props.item.name}</Typography>
+      <br/>
+      <Typography variant="h4" align="center"
+                  color="secondary">{props.item.description}</Typography>
+    </Paper>
   )
 }
 
--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,12 +1,38 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 
+import SternbachOtf from './fonts/Sternbach.otf'
+
+const sternbach = {
+  fontFamily: 'Sternbach',
+  fontStyle: 'normal',
+  fontDisplay: 'swap',
+  fontWeight: 400,
+  src: `
+    local('Sternbach'),
+    local('Sternbach-Regular'),
+    url(${SternbachOtf}) format('otf')
+  `,
+  unicodeRange:
+    'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF'
+}
+
 export default createMuiTheme({
+  typography: {
+    fontFamily: 'Sternbach, Segoe UI'
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '@font-face': [sternbach]
+      }
+    }
+  },
   palette: {
     primary: {
       main: '#000000'
     },
     secondary: {
-      main: '#003d72'
+      main: '#FFFFFF'
     },
     error: {
       main: '#c1c1c1'
